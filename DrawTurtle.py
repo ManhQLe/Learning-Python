8cf27f5bf39f2cd2d103f1039d02d8a904992c29
Manh Le <quang.manh@gmail.com>
2018-03-06 22:27:39 -0600

    Draw circling square

--- DrawTurtle.py
+++ DrawTurtle.py
@@ -1,20 +1,35 @@
 import turtle
 
-def draw_square():
+def draw_square(turt):
+    for i in range(1,5):
+        turt.forward(100)
+        turt.right(90)
+
+def draw_art():
     window = turtle.Screen()
     window.bgcolor("#f0f0f0")
     manh = turtle.Turtle()
     manh.shape("turtle")
     manh.color("#2ecc71")
 
-    manh.forward(100)
-    manh.right(90)
-    manh.forward(100)
-    manh.right(90)
-    manh.forward(100)
-    manh.right(90)
-    manh.forward(100)
-    manh.right(90)
+    draw_square(manh)
+
+    tuan = turtle.Turtle()
+    tuan.shape("triangle")
+    tuan.color("#9b59b6")
+    tuan.circle(50)
     window.exitonclick()
 
-draw_square()
+def draw_cirsquare():
+    window = turtle.Screen()
+    window.bgcolor("#f0f0f0")
+    manh = turtle.Turtle()
+    manh.shape("turtle")
+    manh.color("#2ecc71") 
+    x = int(360/10+1)
+    for i in range(1,x):        
+        draw_square(manh)
+        manh.right(10)
+    window.exitonclick()
+
+draw_cirsquare()
