e1aa0d3fe9c13ceeac437291e5b7f7604954d38a
Manh Le <quang.manh@gmail.com>
2018-03-08 20:53:49 -0600

    Finished Website building

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,5 +1,5 @@
 import media
-
+import fresh_tomatoes
 toy_story = media.Movie(
     "Toy Story",
     "A story of a boy and his toys which come to life",
@@ -7,5 +7,15 @@ toy_story = media.Movie(
     "https://www.youtube.com/watch?v=QW0sjQFpXTU"
 )
 
+avatar = media.Movie(
+    "Avatar",
+    "Imaginary planet",
+    "https://cdn.traileraddict.com/content/20th-century-fox/avatar-6.jpg",
+    "https://www.youtube.com/watch?v=5PSNL1qE6VY"
+)
+
 
 print toy_story.storyline
+toy_story.show_trailer()
+
+fresh_tomatoes.open_movies_page([avatar,toy_story])
--- media.py
+++ media.py
@@ -1,6 +1,11 @@
+import webbrowser
+
 class Movie():
     def __init__(self,movie_title,story_line,poster_url,trailer_url):
         self.title = movie_title
         self.storyline = story_line
         self.poster_image_url = poster_url
         self.trailer_youtube_url = trailer_url
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
