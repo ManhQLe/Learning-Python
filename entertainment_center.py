aa3b59728f3013e31f11dd2091cdf545e8250430
Manh Le <quang.manh@gmail.com>
2018-03-08 20:31:29 -0600

    Added Movie class

---
+++ entertainment_center.py
@@ -0,0 +1,11 @@
+import media
+
+toy_story = media.Movie(
+    "Toy Story",
+    "A story of a boy and his toys which come to life",
+    "https://i.imgur.com/XyxAQqc.jpg",
+    "https://www.youtube.com/watch?v=QW0sjQFpXTU"
+)
+
+
+print toy_story.storyline
---
+++ media.py
@@ -0,0 +1,6 @@
+class Movie():
+    def __init__(self,movie_title,story_line,poster_url,trailer_url):
+        self.title = movie_title
+        self.storyline = story_line
+        self.poster_image_url = poster_url
+        self.trailer_youtube_url = trailer_url
