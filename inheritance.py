ce8da0b8c1f726fd5c8b942bbf01dbe89d0715f1
Manh Le <quang.manh@gmail.com>
2018-03-09 23:01:41 -0600

    Inheritance method call

--- inheritance.py
+++ inheritance.py
@@ -5,13 +5,25 @@ class Parent():
         self.last_name = last_name
         self.eye_color = eye_color
 
+    def show_info(self):
+        print("Last Name: " +self.last_name)
+        print("Eye Color: " +self.eye_color)
+
 class Child(Parent):
     def __init__(self,last_name,eye_color,number_of_toys):
         print ("Child constructor called")
         Parent.__init__(self,last_name,eye_color)
         self.number_of_toys = number_of_toys
 
+    def show_info(self):
+        Parent.show_info(self)
+        print("Number of toys: " + self.number_of_toys)
+
+
 b = Parent("Manh","Black")
 
 c = Child("Manh","Black","Infinitity")
+c.show_info()
+
+
 
