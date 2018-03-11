a3fc2df8633921bd19452583b63e9479a6b6a154
Manh Le <quang.manh@gmail.com>
2018-03-11 00:10:05 -0600

    Elementwise numpy add

---
+++ numpy3.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+m1 = np.array([1,2,3,4])
+
+m2 = np.array([5,6,7,8])
+
+m3 = m1 + m2
+
+m4 = np.array([
+    [1,2],
+    [3,4]
+])
+
+m5 = np.array([
+    [5,6],
+    [7,8]
+])
+
+m6 = m4 + m5
+
+
+print(m3)
+
+print(m6)
